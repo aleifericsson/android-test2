855381fa6730a7988c9684d06200a342496e002a
Alif <134204563+aleifericsson@users.noreply.github.com>
2024-04-01 11:33:48 +0100

    memes

--- app/src/main/java/com/example/second/MainActivity.kt
+++ app/src/main/java/com/example/second/MainActivity.kt
@@ -5,7 +5,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,7 +21,10 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Card
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,6 +39,12 @@ import androidx.compose.ui.unit.dp
 import com.example.second.data.Datasource
 import com.example.second.model.CardData
 import com.example.second.ui.theme.SecondTheme
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +68,7 @@ fun MyApp() {
     CardList(
         cardList = Datasource().loadCards()
     )
+    CookieClicker()
 }
 
 @Composable
@@ -68,8 +85,21 @@ fun CardList(cardList: List<CardData>, modifier:Modifier = Modifier){
 
 @Composable
 fun MyCard(cardData: CardData, modifier: Modifier = Modifier){
+    var expanded by remember {mutableStateOf(false)}
+    val color by animateColorAsState(
+        targetValue = if(expanded) MaterialTheme.colorScheme.tertiaryContainer
+        else MaterialTheme.colorScheme.primaryContainer,
+    )
     Card(modifier = modifier){
-        Column{
+        Column(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMedium
+                )
+            )
+                .background(color = color)
+        ){
             Image(
                 painter = painterResource(id = cardData.imageResourceId),
                 contentDescription = stringResource(id = cardData.stringResourceId),
@@ -78,12 +108,68 @@ fun MyCard(cardData: CardData, modifier: Modifier = Modifier){
                     .fillMaxWidth()
                     .height(194.dp)
             )
-            Text(
-                text = LocalContext.current.getString(cardData.stringResourceId),
-                style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(16.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
             )
+            {
+                Spacer(modifier = Modifier.weight(1f))
+                MyItemButton(
+                    expanded = expanded,
+                    onClick = {
+                        expanded = !expanded
+                    })
+            }
+            if (expanded){
+                MyText(str = LocalContext.current.getString(cardData.stringResourceId))
+            }
         }
+    }
+}
+
+@Composable
+private fun CookieClicker(
+    modifier: Modifier = Modifier
+){
+
+}
+
+@Composable
+private fun Cookie(
+
+){
+
+}
+
+@Composable
+fun MyText(
+    str: String,
+    modifier: Modifier = Modifier
+){
+    Text(
+        text = str,
+        style = MaterialTheme.typography.headlineSmall,
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+@Composable
+private fun MyItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ){
+        Icon(
+            imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+            contentDescription = stringResource(id = R.string.expand),
+            tint = MaterialTheme.colorScheme.secondary
+        )
     }
 }
 
